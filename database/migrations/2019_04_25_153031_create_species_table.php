de366691d788e090eb87fcfd8625294f9114c6b3
KseniyaV <kseniyaverenich@gmail.com>
2019-04-25 13:37:35 -0400

    created species schema

---
+++ database/migrations/2019_04_25_153031_create_species_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpeciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('species', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->binary('photo')->nullable();
+            $table->string('common');
+            $table->string('latin');
+            $table->string('description');
+            $table->string('watering');
+            $table->string('light');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('species');
+    }
+}
